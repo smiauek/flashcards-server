1a666a9b02563f993cf89ee23392a10d355033ea
Piotr Smialkowski <smiaucius@gmail.com>
2022-06-15 14:05:09 -0500

    minimal code corrections

--- src/main/java/com/example/flashcards/controllers/DeckController.java
+++ src/main/java/com/example/flashcards/controllers/DeckController.java
@@ -13,11 +13,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.flashcards.entities.CardEntity;
 import com.example.flashcards.entities.DeckEntity;
 import com.example.flashcards.repositories.DeckRepository;
 
@@ -39,19 +41,32 @@ public class DeckController {
 
 		return deckRepo.findById(id);
 	}
-	
-	@PostMapping(path = "/new", 
-	        consumes = MediaType.APPLICATION_JSON_VALUE, 
-	        produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@PostMapping(path = "/new", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<DeckEntity> create(@RequestBody DeckEntity newDeck) throws ServerException {
-	    DeckEntity deck = deckRepo.save(newDeck);
-	    if (deck == null) {
-	        throw new ServerException("something went wron when creating new deck");
-	    } else {
-	        return new ResponseEntity<DeckEntity>(deck, HttpStatus.CREATED);
-	    }
+		DeckEntity deck = deckRepo.save(newDeck);
+		if (deck == null) {
+			throw new ServerException("something went wron when creating new deck");
+		} else {
+			return new ResponseEntity<DeckEntity>(deck, HttpStatus.CREATED);
+		}
 	}
-	
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<DeckEntity> updateDeck(@PathVariable Long id, @RequestBody DeckEntity updatedDeck)
+			throws Exception {
+
+		DeckEntity deckToUpdate = deckRepo.findById(id).orElseThrow(() -> new Exception("deck not found"));
+
+		deckToUpdate.setName(updatedDeck.getName());
+		deckToUpdate.setDescription(updatedDeck.getDescription());
+
+		DeckEntity deck = deckRepo.save(deckToUpdate);
+
+		return ResponseEntity.ok(deck);
+
+	}
+
 	@DeleteMapping("/{id}")
 	public void deleteDeck(@PathVariable Long id) {
 		deckRepo.deleteById(id);
